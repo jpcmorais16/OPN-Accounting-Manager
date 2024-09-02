4d914104d070c15cfd28892f2d06621296224396
jpcmorais16 <96194625+jpcmorais16@users.noreply.github.com>
2024-09-01 23:21:16 -0300

    counting results

--- sheets.py
+++ sheets.py
@@ -29,16 +29,10 @@ def get_counted_products() -> list[Product]:
     return result
 
 
-def read_cb_database() -> list[Product_db]:
+def open_database(sheet):
     try:
-        df = pd.read_excel("Banco de dados.xlsx", sheet_name='CB')
-        df['CB'] = pd.to_numeric(df['CB'], errors='coerce')
-        df = df.sort_values('CB')
-        df = df[df['CB'].notna()]
-        df = df[df['Item'].notna()]
-        
-        result = [Product_db(row['CB'], row['Item'], row['Tipo']) for _, row in df.iterrows()]
-        return result
+        df = pd.read_excel("Banco de dados.xlsx", sheet_name=sheet)
+        return df
     
     except FileNotFoundError:
         print("O arquivo de banco de dados da aplicação não foi encontrado")
@@ -48,6 +42,92 @@ def read_cb_database() -> list[Product_db]:
         print("Erro no banco de dados, chama um computeiro para analisar!")
         print("Erro: ", e)
         exit()
+
+
+def read_cb_database() -> list[Product_db]:
+
+    df = open_database('CB')
+    df['CB'] = pd.to_numeric(df['CB'], errors='coerce')
+    df = df.sort_values('CB')
+    df = df[df['CB'].notna()]
+    df = df[df['Item'].notna()]
+    
+    result = [Product_db(row['CB'], row['Item'], row['Tipo']) for _, row in df.iterrows()]
+    return result
+
+def calculate_weighted_foods_total_weight(counted_products: list[Product]) -> float:
+
+    df = open_database('Alimentos (peso)')
+    cbs = open_database('CB')
+    result = 0
+    
+    for product in counted_products:
+
+        aux1 = cbs[cbs['CB'] == product.cb]
+
+        if(len(aux1) > 0):
+            aux2 = df[df['Item'] == aux1.iloc[0]['Item']]
+
+            if(len(aux2) > 0):
+                result += aux2.iloc[0]['kg/embalagem'] * product.amount
+
+    return result
+
+def calculate_volumed_foods_total_volume(counted_products: list[Product]) -> float:
+
+    df = open_database('Alimentos (volume)')
+    cbs = open_database('CB')
+    result = 0
+
+    for product in counted_products:
+
+        aux1 = cbs[cbs['CB'] == product.cb]
+
+        if(len(aux1) > 0):
+            aux2 = df[df['Item'] == aux1.iloc[0]['Item']]
+
+            
+            if(len(aux2) > 0):
+                result += aux2.iloc[0]['L/embalagem'] * product.amount
+
+    return result
+
+def calculate_weighted_cleaning_total_weight(counted_products: list[Product]) -> float:
+
+    df = open_database('Limpeza (peso)')
+    cbs = open_database('CB')
+    result = 0
+
+    for product in counted_products:
+
+        aux1 = cbs[cbs['CB'] == product.cb]
+
+        if(len(aux1) > 0):
+            aux2 = df[df['Item'] == aux1.iloc[0]['Item']]
+
+
+            if(len(aux2) > 0):
+                result += aux2.iloc[0]['kg/embalagem'] * product.amount
+
+    return result
+
+
+def calculate_volumed_cleaning_total_volume(counted_products: list[Product]) -> float:
+
+    df = open_database('Limpeza (volume)')
+    cbs = open_database('CB')
+    result = 0
+
+    for product in counted_products:
+
+        aux1 = cbs[cbs['CB'] == product.cb]
+        if(len(aux1) > 0):
+            aux2 = df[df['Item'] == aux1.iloc[0]['Item']]
+
+            if(len(aux2) > 0):
+                result += aux2.iloc[0]['L/embalagem'] * product.amount
+
+    return result
 
 
 def persist_new_product(product: Product_db):
@@ -106,21 +186,23 @@ def generate_result_sheet(counted_products: list[Product], cb_name_list: list[tu
     
 
     # Página geral (todos os produtos)
-    main_page = pd.DataFrame({'CB': [], 'Item': [], 'Tipo': [], 'Supermercado': [], 'Turno': [], 'Quantidade': []})
+    main_page = pd.DataFrame({'CB': [], 'Item': [], 'Tipo': [], 'Quantidade': []})
 
     for cb in cb_hash:
 
         name, type, products = cb_hash[cb]
 
-        for product in products:
-            main_page.loc[len(main_page)] = [cb, name, type, product.supermarket, product.shift, product.amount]
+        total_amount = reduce(lambda x, product: x + product.amount, products, 0)
+        main_page.loc[len(main_page)] = [cb, name, type, total_amount]
 
         main_page = main_page.sort_values('Tipo')
        
 
     # Páginas de tipo
+    # TODO incluir o peso parcial de cada produto
 
-    weight_food_page = pd.DataFrame({'Item': [], 'Supermercado': [], 'Quantidade': []})
+
+    weight_food_page = pd.DataFrame({'Item': [], 'Supermercado': [], 'Quantidade': []}) 
     volume_food_page = pd.DataFrame({'Item': [], 'Supermercado': [], 'Quantidade': []})
     weight_cleaning_page = pd.DataFrame({'Item': [], 'Supermercado': [], 'Quantidade': []})
     volume_cleaning_page = pd.DataFrame({'Item': [], 'Supermercado': [], 'Quantidade': []})
@@ -178,14 +260,19 @@ def generate_result_sheet(counted_products: list[Product], cb_name_list: list[tu
                     supermarket_dfs[supermarket].loc[len(supermarket_dfs[supermarket])] = [name, product.shift, product.amount]
 
 
+        supermarket_dfs[supermarket].sort_values('Turno')
+
+
 
     # Página de resultados
 
-    #products_from_db = read_cb_database()
 
     total_amount = reduce(lambda x, p: x + p.amount, counted_products, 0)
     different_products = len(cb_hash)
-
+    weight_food_sum = calculate_weighted_foods_total_weight(counted_products)
+    volume_food_sum = calculate_volumed_foods_total_volume(counted_products)
+    weight_cleaning_sum = calculate_weighted_cleaning_total_weight(counted_products)   
+    volume_cleaning_sum = calculate_volumed_cleaning_total_volume(counted_products)
 
 
 
@@ -207,6 +294,16 @@ def generate_result_sheet(counted_products: list[Product], cb_name_list: list[tu
 
             df.to_excel(writer, sheet_name=supermarket, index=False)
 
+        print(weight_cleaning_sum)
+
+        general_results = pd.DataFrame({"Quantidade de produtos": [total_amount],
+                                        "Quantidade de códigos de barras": [different_products],
+                                        "Soma de peso de alimentos (kg)": [weight_food_sum],
+                                        "Soma de volume de alimentos (L)": [volume_food_sum],
+                                        "Soma de peso de produtos de limpeza (kg)": [weight_cleaning_sum],
+                                        "Soma de volume de produtos de limpeza (L)": [volume_cleaning_sum]})
+
+        general_results.to_excel(writer, sheet_name="Resultados Gerais", index=False)
         
 
 
